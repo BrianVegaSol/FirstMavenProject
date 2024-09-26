9e7f62cffb7f422888d1cda0d4c1525cb5c5d7d3
BrianVegaSol <brivegasol@gmail.com>
2024-09-26 13:50:48 -0700

    First iteration

---
+++ src/main/java/com/pluarlsight/Main.java
@@ -0,0 +1,7 @@
+package com.pluarlsight;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/pluarlsight/ShoppingList.java
@@ -0,0 +1,25 @@
+package com.pluarlsight;
+
+import java.lang.reflect.Array;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println(
+                "Bacon\n"+
+                "Eggs\n"+
+                "Cheese\n"+
+                "Potatoes\n"+
+                 "Grapes\n"+
+                 "Soda\n"+
+                 "Broccoli\n"+
+                 "Celery\n"+
+                  "Chicken\n"+
+                  "Juice"
+        );
+        String[] shoppingList = {"s", "Potoato"};
+        System.out.println(shoppingList[1]);
+
+        //System.out.println(Array.toString(shoppingList[]));
+    }
+
+}
